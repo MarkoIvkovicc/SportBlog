11de832bfeb2f6969cc46379cdc69205ec7ad20e
zivkoobradovic <37649807+zivkoo@users.noreply.github.com>
2020-08-17 11:38:16 +0200

    deleted em from postcontroller

--- app/Controllers/PostController.php
+++ app/Controllers/PostController.php
@@ -10,32 +10,7 @@ use Doctrine\ORM\EntityManager;
 
 class PostController
 {
-    private function em()
-    {
-      // Get connection variables
-      $app = require 'config.php';
-
-      // Set connection variables
-      $params = array(
-          'dbname' => $app['name'],
-          'user' => $app['username'],
-          'password' => $app['password'],
-          'host' => $app['host'],
-          'port' => $app['port'],
-          'driver' => $app['driver'],
-          'path' => __DIR__ . '/db.mysql',
-      );
-
-      $isDevMode = true;
-      $proxyDir = null;
-      $cache = null;
-      $useSimpleAnnotationReader = false;
-      $config = Setup::createAnnotationMetadataConfiguration(array("app/src"), $isDevMode, $proxyDir, $cache, $useSimpleAnnotationReader);
-
-      // Obtaining the entity manager
-      return EntityManager::create($params, $config);
-    }
-
+    
     public function index () {
       echo twig()->render('index.html');
     }
