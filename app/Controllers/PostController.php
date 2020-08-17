32b316a08a7047bc488209574ecaba7ee1c12927
Marko Ivković <MarkoIvkovicc@users.noreply.github.com>
2020-08-17 12:09:36 +0200

    fix: conflict resolved

--- app/Controllers/PostController.php
+++ app/Controllers/PostController.php
@@ -13,6 +13,8 @@ class PostController
     
     public function index () {
       echo twig()->render('index.html');
+      $posts = $this->em()->getRepository('App\src\Post')->findAll();
+      echo twig()->render('index.html', compact('posts'));
     }
 
     public function create () {
@@ -23,9 +25,6 @@ class PostController
       $post = new Post;
       $post->setTitle(request()->get('title'));
       $post->setBody(request()->get('body'));
-      $post->setCreatedAt(new \DateTime('now'));
-
-      
 
       em()->persist($post);
       em()->flush();
@@ -36,18 +35,32 @@ class PostController
     public function show ($id) 
     {
       em()->find('app\src\Post', $id);
+      echo twig()->render('posts/post-show.html', compact('post'));
+    }
+
+    public function edit ($id)
+    {
+      $post = $this->em()->find('App\src\Post', $id);
+      echo twig()->render('posts/post-edit.html', compact('post'));
     }
 
     public function update ($id)
     {
-      /* em()->merge($post); */
+      $post = em()->find('App\src\Post', $id);
+
+      $post->getTitle() != request()->get('title') ? $post->setTitle(request()->get('title')) : '';
+      $post->getBody() != request()->get('body') ? $post->setBody(request()->get('body')) : '';
+      $post->setCreatedAt(new \DateTime('now'));
+      em()->merge($post);
       em()->flush();
+
+      return header("Location: /posts/".$post->getId());
     }
 
     public function delete ($id)
     {
       $post = new Post;
-      $post = $this->em()->find('app\src\Post', $id);
+      $post = em()->find('app\src\Post', $id);
       em()->remove($post);
       em()->flush();
 
--- routes.php
+++ routes.php
@@ -5,3 +5,5 @@ $router->get('/', '\App\Controllers\PostController@index');
 $router->get('/posts/create', '\App\Controllers\PostController@create');
 $router->get('/posts/{id}', '\App\Controllers\PostController@show');
 $router->post('/posts', '\App\Controllers\PostController@store');
+$router->post('/updatePost/{id}', '\App\Controllers\PostController@update');
+$router->post('/deletePost/{id}', '\App\Controllers\PostController@delete');
