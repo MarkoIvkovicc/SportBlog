956b66e0d2943352d93815a5d7f58abfa70f2527
Marko Ivković <MarkoIvkovicc@users.noreply.github.com>
2020-08-18 14:46:08 +0200

    feat: PostController

--- app/Controllers/PostController.php
+++ app/Controllers/PostController.php
@@ -29,7 +29,7 @@ class PostController
 
     public function show ($id) 
     {
-      em()->find('app\src\Post', $id);
+      $post = em()->find('App\src\Post', $id);
       echo twig()->render('posts/post-show.html', compact('post'));
     }
 
@@ -46,6 +46,7 @@ class PostController
       $post->getTitle() != request()->get('title') ? $post->setTitle(request()->get('title')) : '';
       $post->getBody() != request()->get('body') ? $post->setBody(request()->get('body')) : '';
       $post->setCreatedAt(new \DateTime('now'));
+      
       em()->merge($post);
       em()->flush();
 
@@ -56,6 +57,7 @@ class PostController
     {
       $post = new Post;
       $post = em()->find('app\src\Post', $id);
+
       em()->remove($post);
       em()->flush();
 
--- routes.php
+++ routes.php
@@ -6,6 +6,7 @@ $router->get('/', '\App\Controllers\PostController@index');
 $router->get('/posts/create', '\App\Controllers\PostController@create');
 $router->get('/posts/{id}', '\App\Controllers\PostController@show');
 $router->post('/posts', '\App\Controllers\PostController@store');
+$router->get('/editPost/{id}', '\App\Controllers\PostController@edit');
 $router->post('/updatePost/{id}', '\App\Controllers\PostController@update');
 $router->post('/deletePost/{id}', '\App\Controllers\PostController@delete');
 
