4871d706f4973ace271adcce78f8cc9f15aac422
Marko Ivković <MarkoIvkovicc@users.noreply.github.com>
2020-09-15 14:59:49 +0200

    feat: WIP lead card

--- app/Controllers/HomepageController.php
+++ app/Controllers/HomepageController.php
@@ -2,19 +2,34 @@
 
 namespace App\Controllers;
 
-use App\Models\User;
+use App\src\User;
+use App\Models\User as UserModel;
 
 class HomepageController {
     public function index () {
-    	session_start();
+
+        $queryLast4 = 'SELECT * FROM Posts ORDER BY id DESC LIMIT 4';
+        
+        $statLast4 = em()->getConnection()->prepare($queryLast4);
+        $statLast4->execute();
+
+        $fetchLast4 = $statLast4->fetchAll();
+
+        foreach ($fetchLast4 as $key => $value) {
+            $usersId = array_shift(em()->getRepository(User::class)->findBy(array('id' => $value['owner_id'])));
+            $usersImg[] = $usersId->getImage();
+            $usersName[] = $usersId->getName();
+        }
+
+    	// session_start();
     	$admin = $logged = null;
 
     	if (isset($_SESSION['token'])) {
-	    	$user = new User;
+	    	$user = new UserModel;
 	    	$user->isAdmin() ? $admin = true : '';
 	    	isset($user) ? $logged = true : $logged = false;
     	}
 
-        echo twig()->render('homepage.html', compact('admin', 'logged')); 
+        echo twig()->render('homepage.html', compact('admin', 'logged', 'fetchLast4', 'usersImg', 'usersName')); 
     }
 } 
